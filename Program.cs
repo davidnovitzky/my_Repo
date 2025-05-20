3fbfe050dd215ea554c54848196c6d2ddef15ea9
davidnovitzky <deivido.navicko@gmail.com>
2025-05-21 00:25:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trip_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double distance = 0.0; // <- double float variable for decimals
+            double consumption = 0.0;
+            double fuelPrice = 0.0;
+   
+            Console.Write($"Distance (km): ");
+            distance = float.Parse(Console.ReadLine()); // <- string to float conversion
+            
+            Console.Write("Fuel Consumption (L/100km): ");
+            consumption = float.Parse(Console.ReadLine());
+
+            Console.Write("Fuel Price ($): ");
+            fuelPrice = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("--- Totals ---");
+
+            double fuelNeeded = (distance / 100.0) * consumption;
+            double fuelCost = fuelNeeded * fuelPrice;
+
+            Console.WriteLine($"Total Fuel Needed: {fuelNeeded:F2} liters"); // <- F2  2 decimal points
+            Console.WriteLine($"Total trip cost: ${fuelCost:F2}");
+            Console.ReadLine();
+        }
+    }
+}
