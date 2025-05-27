136684525d4ab4a1c896230518691281d2279191
davidnovitzky <deivido.navicko@gmail.com>
2025-05-27 09:16:32 +0300

    trip_Calculator.cs

--- trip_Calculator.cs
+++ trip_Calculator.cs
@@ -1,32 +1,21 @@
-﻿using System;
-
-namespace Trip_Calculator
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            double distance = 0.0; // <- double float variable for decimals
-            double consumption = 0.0;
-            double fuelPrice = 0.0;
+double distance = 0.0; // <- double float variable for decimals
+double consumption = 0.0;
+double fuelPrice = 0.0;
    
-            Console.Write($"Distance (km): ");
-            distance = float.Parse(Console.ReadLine()); // <- string to float conversion
+Console.Write($"Distance (km): ");
+distance = float.Parse(Console.ReadLine()); // <- string to float conversion
             
-            Console.Write("Fuel Consumption (L/100km): ");
-            consumption = float.Parse(Console.ReadLine());
+Console.Write("Fuel Consumption (L/100km): ");
+consumption = float.Parse(Console.ReadLine());
 
-            Console.Write("Fuel Price ($): ");
-            fuelPrice = float.Parse(Console.ReadLine());
+Console.Write("Fuel Price ($): ");
+fuelPrice = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("--- Totals ---");
+Console.WriteLine("--- Totals ---");
 
-            double fuelNeeded = (distance / 100.0) * consumption;
-            double fuelCost = fuelNeeded * fuelPrice;
+double fuelNeeded = (distance / 100.0) * consumption;
+double fuelCost = fuelNeeded * fuelPrice;
 
-            Console.WriteLine($"Total Fuel Needed: {fuelNeeded:F2} liters"); // <- F2  2 decimal points
-            Console.WriteLine($"Total trip cost: ${fuelCost:F2}");
-            Console.ReadLine();
-        }
-    }
-}
+Console.WriteLine($"Total Fuel Needed: {fuelNeeded:F2} liters"); // <- F2  2 decimal points
+Console.WriteLine($"Total trip cost: ${fuelCost:F2}");
+Console.ReadLine();
