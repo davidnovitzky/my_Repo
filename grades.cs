54206dbce5ea14576ba0808308ee297cbd2ab72a
davidnovitzky <deivido.navicko@gmail.com>
2025-05-27 08:08:21 +0300

    Add files via upload

---
+++ grades.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+
+Console.Write("How many students? ");
+int studentCount = int.Parse(Console.ReadLine());
+
+int[] grades = new int[studentCount];
+
+float total = 0;
+int currentSmallest = int.MaxValue;
+int currentLargest = int.MinValue;
+
+for (int i = 0; i < grades.Length; i++)
+{
+    Console.Write($"Enter grade for student {i+1}: ");
+    grades[i] = int.Parse(Console.ReadLine());
+
+    if (grades[i] < currentSmallest)
+    {
+        currentSmallest = grades[i];
+    }
+    if (grades[i] > currentLargest)
+    {
+        currentLargest = grades[i];
+    }
+    total = total + grades[i];
+}
+
+float average = total / studentCount;
+
+Console.WriteLine();
+Console.WriteLine($"Average grade: {average.ToString("F2")}");
+Console.WriteLine($"Highest grade: {currentLargest}");
+Console.WriteLine($"Lowest grade: {currentSmallest}");
+Console.ReadLine();
