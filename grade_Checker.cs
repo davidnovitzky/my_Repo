ea322be9c63ed2e099927d5142e2390b237c2f09
davidnovitzky <deivido.navicko@gmail.com>
2025-05-27 09:19:50 +0300

    grade_Checker.cs

--- grade_Checker.cs
+++ grade_Checker.cs
@@ -1,44 +1,33 @@
-﻿using System;
-
-namespace Grade_Checker
+// create an integer variable for score
+int score = 0;
+// print your score
+Console.Write($"Enter your score: ");
+// convert string input to integer variable
+// creater an input for score inside the integer variable
+score = Int32.Parse(Console.ReadLine());
+// if score is greater than 90 print Excellent
+if (score >= 90 && score <= 100)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // create an integer variable for score
-            int score = 0;
-            // print your score
-            Console.Write($"Enter your score: ");
-            // convert string input to integer variable
-            // creater an input for score inside the integer variable
-            score = Int32.Parse(Console.ReadLine());
-            // if score is greater than 90 print Excellent
-            if (score >= 90 && score <= 100)
-            {
-                Console.WriteLine("Excellent!");
-            }
-            // if the score is between 70 and 89 print Good Job
-            else if (score >= 70 && score <= 89)
-            {
-                Console.WriteLine("Good Job!");
-            }
-            // if the score is between 50 and 69 print You passed
-            else if (score >= 50 && score <= 69)
-            {
-                Console.WriteLine("You passed!");
-            }
-            // if the score is below 50 print You failed
-            else if (score >= 0 && score <= 49)
-            {
-                Console.WriteLine("You failed");
-            }
-            // if the score is any number above 100 or below 0 print invalid
-            else
-            {
-                Console.WriteLine("Invalid Score");
-            }
-                Console.ReadLine();
-        }
-    }
+    Console.WriteLine("Excellent!");
 }
+// if the score is between 70 and 89 print Good Job
+else if (score >= 70 && score <= 89)
+{
+    Console.WriteLine("Good Job!");
+}
+// if the score is between 50 and 69 print You passed
+else if (score >= 50 && score <= 69)
+{
+    Console.WriteLine("You passed!");
+}
+// if the score is below 50 print You failed
+else if (score >= 0 && score <= 49)
+{
+    Console.WriteLine("You failed");
+}
+// if the score is any number above 100 or below 0 print invalid
+else
+{
+    Console.WriteLine("Invalid Score");
+}
+Console.ReadLine();
