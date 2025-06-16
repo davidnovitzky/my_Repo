15c43abe0006784431891286ba7cc8f24f52971a
davidnovitzky <deivido.navicko@gmail.com>
2025-06-16 15:43:11 +0300

    Update traffic_light_enum.cs

--- traffic_light_enum.cs
+++ traffic_light_enum.cs
@@ -9,31 +9,34 @@ while (true)
     switch (lightState)
     {
         case TrafficLightState.Red:
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"{lightState}");
-            Console.ResetColor();
+            ShowLightState();
             Console.WriteLine("Stop and wait!");
             CountdownTimerInSeconds(5);
             lightState = TrafficLightState.Yellow;
             break;
         case TrafficLightState.Yellow:
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine($"{lightState}");
-            Console.ResetColor();
+            ShowLightState();
             Console.WriteLine("Prepare to go.");
             CountdownTimerInSeconds(3);
             lightState = TrafficLightState.Green;
             break;
         case TrafficLightState.Green:
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"{lightState}");
-            Console.ResetColor();
+            ShowLightState();
             Console.WriteLine("Go go go!");
             CountdownTimerInSeconds(5);
             Console.WriteLine("Stop!");
             lightState = TrafficLightState.Red;
             break;
     }
+}
+void ShowLightState()
+{
+    if      (lightState == TrafficLightState.Red)    Console.ForegroundColor = ConsoleColor.Red;
+    else if (lightState == TrafficLightState.Yellow) Console.ForegroundColor = ConsoleColor.Yellow;
+    else if (lightState == TrafficLightState.Green)  Console.ForegroundColor = ConsoleColor.Green;
+
+    Console.WriteLine($"{lightState}");
+    Console.ResetColor();
 }
 void CountdownTimerInSeconds(int seconds)
 {
