92539d92485f29a064e94e47d2015578e104aa05
davidnovitzky <deivido.navicko@gmail.com>
2025-06-22 13:07:47 +0300

    Create student_tracker.cs

---
+++ student_tracker.cs
@@ -0,0 +1,13 @@
+Create a simple program that stores students' names along with their grades in various subjects using tuples. Then display a report for each student.
+
+Store each student's data as a tuple
+string name, string subject, string grade
+Store multiple students in an array
+
+1. Allow the user to view all students first
+Pick one student out of total
+Show selected student grades of all subjects
+2. View a students grade
+Enter students name
+3. Add a student with grades for each subject
+4.Print a report that lists all subjects and the grade for each student.
