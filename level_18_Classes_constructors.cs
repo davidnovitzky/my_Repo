a1ca0a80049a0f61e2d58276c7de507ec45770bd
davidnovitzky <deivido.navicko@gmail.com>
2025-06-23 22:36:06 +0300

    Update level_18_Classes_constructors.cs

--- level_18_Classes_constructors.cs
+++ level_18_Classes_constructors.cs
@@ -1,16 +1,17 @@
-Pet newCat = new(PetType.Cat, "Bagira", 8, 8); // calling the constructor
+Pet newCat = new(PetType.Cat, "Bagira", 8, 8); // calling the constructor, used to set up the object’s data.
 
-class Pet
+class Pet // class - blueprint to create objects
 {
     public PetType Type;
-    public string Name;
-    public int Age;
+    public string Name; // fields - variable inside a class to store data about the object
+    public int Age;     // fields - belong to the object
     public int Weight;
 
+    // The parameters (string name, etc.) come from outside
     public Pet(PetType type, string name, int age, int weight) // defining the constructor
     {
         Type = type;
-        Name = name;
+        Name = name; // Assigns the input value to the object’s field when calling the constructor
         Age = age;
         Weight = weight;
     }
