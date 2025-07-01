b37ae81de435563a2ca6f1565e235b4e06b826ca
davidnovitzky <deivido.navicko@gmail.com>
2025-07-01 14:56:14 +0300

    Update bank_account_static_test.cs

--- bank_account_static_test.cs
+++ bank_account_static_test.cs
@@ -38,7 +38,7 @@ public class BankAccount
         }
         AccountBalance -= withdrawAmount;
     }
-    public static int DisplayTotalAccounts()
+    public static int DisplayTotalAccounts() // not needed anymore since the property already shows that and can be accessed
     {
         return TotalAccountsCreated;
     }
@@ -46,3 +46,48 @@ public class BankAccount
 
 // total number of accounts is shared info about the whole class
 // it belongs to the bank system, not any single account
+
+*** Improved Version ***
+
+BankAccount acc1 = new();
+//acc1.Accountbalance = 50000000; // not allowed
+Console.WriteLine($"{acc1.AccountBalance} is your balance");
+acc1.DepositMoney(4501);
+acc1.WithdrawMoney(5000);
+Console.WriteLine($"{acc1.AccountBalance} is your balance");
+string accountWord = BankAccount.TotalAccountsCreated == 1 ? "account" : "accounts";
+Console.WriteLine($"There are {BankAccount.TotalAccountsCreated} {accountWord} created ");
+
+//BankAccount.TotalAccountsCreated = 5;
+
+public class BankAccount
+{
+    //private static int _totalAccountsCreated;
+    //private int _accountBalance;
+
+    public int AccountBalance
+    {
+        get;
+        private set;
+    }
+    public static int TotalAccountsCreated { get; private set; }
+
+    public BankAccount()
+    {
+        TotalAccountsCreated++;
+    }
+    public void DepositMoney(int depositAmount)
+    {
+        AccountBalance += depositAmount;
+    }
+    public bool WithdrawMoney(int withdrawAmount)
+    {
+        if (AccountBalance < withdrawAmount)
+        {
+            Console.WriteLine("Insufficient funds");
+            return false;
+        }
+        AccountBalance -= withdrawAmount;
+        return true;
+    }
+}
