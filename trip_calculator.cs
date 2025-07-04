e7d3b884f63b16c6eade6cf471d9c4b9d6ae7aff
davidnovitzky <deivido.navicko@gmail.com>
2025-07-04 22:37:56 +0300

    Update trip_calculator.cs

--- trip_calculator.cs
+++ trip_calculator.cs
@@ -1,16 +1,16 @@
 Console.Write($"Distance (km): ");
-double distance = float.Parse(Console.ReadLine()); // <- string to float conversion
+float distance = float.Parse(Console.ReadLine()); // <- string to float conversion
 
 Console.Write("Fuel Consumption (L/100km): ");
-double consumption = float.Parse(Console.ReadLine());
+float consumption = float.Parse(Console.ReadLine());
 
 Console.Write("Fuel Price ($): ");
-double fuelPrice = float.Parse(Console.ReadLine());
+float fuelPrice = float.Parse(Console.ReadLine());
 
 Console.WriteLine("--- Totals ---");
 
-double fuelNeeded = (distance / 100.0) * consumption;
-double fuelCost = fuelNeeded * fuelPrice;
+float fuelNeeded = (distance / 100.0f) * consumption;
+float fuelCost = fuelNeeded * fuelPrice;
 
 Console.WriteLine($"Total Fuel Needed: {fuelNeeded:F2} liters"); // <- F2  2 decimal points
 Console.WriteLine($"Total trip cost: ${fuelCost:F2}");
