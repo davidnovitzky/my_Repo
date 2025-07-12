1b33c8558444d1d6e71be3fa1938eed611ad46b3
davidnovitzky <deivido.navicko@gmail.com>
2025-07-12 13:51:21 +0300

    Update flat_earth.cs

--- flat_earth.cs
+++ flat_earth.cs
@@ -1,4 +1,4 @@
-Console.Write("Is the Earth round?\n1: Yes\n2: No\nAnswer: ");
+Console.Write("Is the Earth flat?\n1: Yes\n2: No\nAnswer: ");
 string userValue = Console.ReadLine();
 int num;
 
@@ -7,6 +7,5 @@ while (!int.TryParse(userValue, out num) || (num != 1 && num != 2))
     Console.Write("Invalid input. Please enter 1 or 2: ");
     userValue = Console.ReadLine();
 }
-string message = num == 1 ? "correct" : "not correct";
-Console.Write($"Your answer is {message}");
-Console.ReadLine();
+string message = num == 1 ? "Your head is flat" : "Correct";
+Console.Write($"{message}");
