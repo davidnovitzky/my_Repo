1855ad5640a7c35bd7ca7d22f800ef74a672e58f
davidnovitzky <deivido.navicko@gmail.com>
2025-07-13 10:01:17 +0300

    Create four_principles_of_OOP.cs

---
+++ four_principles_of_OOP.cs
@@ -0,0 +1,73 @@
+Car corolla = new Car("Toyota", "Corolla");
+Motorcycle ninja = new Motorcycle("Kawasaki", "Ninja");
+
+Vehicle[] vehicles = { corolla, ninja };
+
+foreach (Vehicle vehicle in vehicles)
+{
+    vehicle.Drive(100);
+}
+
+Console.WriteLine();
+
+corolla.Drive(150);
+
+Console.WriteLine();
+
+Console.WriteLine($"The {corolla.Model} has driven {corolla.TotalDistanceDriven}km in total");
+Console.WriteLine($"The {ninja.Model} has driven {ninja.TotalDistanceDriven}km in total");
+
+corolla.ResetCurrentDistance();
+
+Console.WriteLine();
+
+Console.WriteLine($"After reset the {corolla.Model} current distance is {corolla.CurrentDistanceDriven}. And a total of {corolla.TotalDistanceDriven}");
+
+public class Vehicle
+{
+    public string Make { get; }
+    public string Model { get; }
+    public int TotalDistanceDriven { get; private set; } = 0;
+    public int CurrentDistanceDriven { get; private set; } = 0;
+
+    public Vehicle(string make, string model)
+    {
+        Make = make;
+        Model = model;
+    }
+
+    public virtual void Drive(int distance)
+    {    
+        CurrentDistanceDriven += distance;
+        TotalDistanceDriven += distance;
+    }
+    public void ResetCurrentDistance()
+    {
+        CurrentDistanceDriven = 0;
+    }
+}
+public class Car : Vehicle
+{
+    public Car(string make, string model) : base(make, model)
+    {
+
+    }
+    public override void Drive(int distance)
+    {
+        Console.WriteLine($"The {Model} is driving for {distance} km");
+        base.Drive(distance);
+    }
+}
+    
+public class Motorcycle : Vehicle
+{
+   public Motorcycle(string make, string model) : base(make, model)
+    {
+
+    }
+    public override void Drive(int distance)
+    {
+        Console.WriteLine($"The {Model} is riding for {distance} km");
+        base.Drive(distance);
+    }
+}
