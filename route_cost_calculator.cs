8edcbe6f0dc3ddfdd03dfcad788dfaa9a7bc76c4
davidnovitzky <deivido.navicko@gmail.com>
2025-07-16 01:23:57 +0300

    Update route_cost_calculator.cs

--- route_cost_calculator.cs
+++ route_cost_calculator.cs
@@ -1,29 +1,26 @@
-Console.WriteLine("Fuel Cost Comparison Between Two Routes\n");
+Console.WriteLine("Fuel Cost Comparison Between Two Routes");
 
-Console.Write("Enter fuel price per liter: ");
-double fuelPrice = Convert.ToDouble(Console.ReadLine());
+float fuelPrice = GetUserInput("Enter the Fuel Price: ");
+float longRoute = GetUserInput("Enter the distance for the longer route (km): ");
+float shortRoute = GetUserInput("Enter the distance for the shorter route (km): ");
+float longRouteConsumption = GetUserInput("Long route consumption (L/100km): ");
+float shortRouteConsumption = GetUserInput("Short route consumption (L/100km): ");
 
-Console.Write("\nEnter distance of longer route (km): ");
-double longRoute = Convert.ToDouble(Console.ReadLine());
+float fuelUsedLongRoute = longRoute / 100.0f * longRouteConsumption;
+float fuelUsedShortRoute = shortRoute / 100.0f * shortRouteConsumption;
 
-Console.Write("Enter fuel consumption of longer route (liters/100km): ");
-double longRouteConsumption = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("\nEnter distance of shorter route (km): ");
-double shortRoute = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("Enter fuel consumption of shorter route (liters/100km): ");
-double shortRouteConsumption = Convert.ToDouble(Console.ReadLine());
-
-double fuelUsedLongRoute = (longRoute / 100.0) * longRouteConsumption;
-double fuelUsedShortRoute= (shortRoute / 100.0) * shortRouteConsumption;
-
-double costLongRoute = fuelUsedLongRoute * fuelPrice;
-double costShortRoute = fuelUsedShortRoute * fuelPrice;
+float costLongRoute = fuelUsedLongRoute * fuelPrice;
+float costShortRoute = fuelUsedShortRoute * fuelPrice;
 
 Console.WriteLine($"\nLong route: {longRoute} km, {longRouteConsumption:F1} L/100km, Cost: {costLongRoute:F2}");
 Console.WriteLine($"Short route: {shortRoute} km, {shortRouteConsumption:F1} L/100km, Cost: {costShortRoute:F2}");
 
-if (costLongRoute < costShortRoute)      Console.WriteLine("The long route is more cost-effective.");
+if (costLongRoute < costShortRoute) Console.WriteLine("The long route is more cost-effective.");
 else if (costShortRoute < costLongRoute) Console.WriteLine("\nThe short route is more cost-effective.");
-else                                     Console.WriteLine("Both routes cost the same.");
+else Console.WriteLine("Both routes cost the same.");
+
+float GetUserInput(string prompt)
+{
+    Console.Write(prompt);
+    return float.Parse(Console.ReadLine());
+}
