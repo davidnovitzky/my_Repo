42329a2fb3905c7167500d884f96245a80899769
davidnovitzky <deivido.navicko@gmail.com>
2025-07-20 15:35:58 +0300

    Update flat_earth.cs

--- flat_earth.cs
+++ flat_earth.cs
@@ -7,7 +7,7 @@ while (!int.TryParse(userValue, out num) || (num != 1 && num != 2))
     Console.Write("Invalid input. Please enter 1 or 2: ");
     userValue = Console.ReadLine();
 }
-string message = num == 1 ? "Your head is flat" : "Correct";
+string message = num == 1 ? "Your head is flat." : "Correct.";
 Console.Write($"{message}");
 
 
